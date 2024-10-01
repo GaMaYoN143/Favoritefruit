f8767e603e537392583cd001c9f03594169223e9
GaMaYoN143 <sairylljaygamayon83@gmail.com>
2024-10-01 11:41:02 +0800

    humana ko sir

---
+++ Task1.py
@@ -0,0 +1,28 @@
+myfavfruits= int(input("akong favorite fruit"))
+NamesOfthefruits = []
+
+for i in range (myfavfruits):
+    fruit = ("Enter the fruits : ")
+    NamesOfthefruits.append(fruit)
+
+print(fruit)
+
+
+for data in fruit:
+     print (data)
+     if data == "banana":
+        break
+     elif data == "apple":
+         print("Happy Eating")
+ 
+     
+
+
+   
+
+
+
+        
+        
+    
+
